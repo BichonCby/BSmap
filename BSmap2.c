9a8caf4254dfc56b39721ba900e2616d1cf2b2c3
Bichon <cyril.bykoff@gmail.com>
2021-02-28 23:26:43 +0100

    maj auto

--- BSmap2.c
+++ BSmap2.c
@@ -1,57 +1,38 @@
-
-#define GTK_USE
-
-#ifdef GTK_USE
-//#include <cairo.h>
 #include <gtk/gtk.h> // on le met là sinon il faut compiler le .h (???)
-#include <glib.h>
-#include <glib/gstdio.h>
-#endif
-#define uint8_t char
-#define bool char
-#define true 1
-#define false 0
-#define toupper(g) g
-#define max(a,b) a>b?a:b
-#include "BSmap.h"
+#include "BSmap2.h"
 
-bool blabla = true;
-//const char *list_command = "PVMRS";
-//const char *list_command = "";//PVMRS";
-
-
-const char * fiforead = "/tmp/RobBSmap"; // du robot vers l'outil
-const char * fifowrite = "/tmp/BSmapRob"; // de l'outil vers le robot
-int fd;
-
-int main(int argc, char **argv)
+int main(int argc,char *argv[])
 {
-#ifdef GTK_USE
-	gtk_init(&argc, &argv);
-#endif
-	//char commande[50];
-	// création des fifo, au cas où elles n'existeraient pas
+	GtkBuilder *builder;
+	GtkWidget *window;
+
 	if (mkfifo(fiforead,0777) != 0 || mkfifo(fifowrite,0777) != 0)
 	{
 		printf("imposible de créer le pipe \n");
 		//return;
 	}
 	printf("pipe OK\n");
-	
-	pthread_create(&inp,NULL,&inputCommand,NULL);
-	pthread_create(&mess,NULL,&manageMessages,NULL);
-	
-	// pour l'instant on est en ligne de commande. On demande donc
-	// une trame ou on donne un ordre par l'entrée strandard
-	createHMI();
-#ifdef GTK_USE
-	gtk_main();
-#else
-	while (1) ;
-#endif
-	return(0);
 
+	pthread_create(&mess,NULL,&manageMessages,NULL);
+	pthread_create(&inp,NULL,&inputCommand,NULL);
+
+	gtk_init(&argc, &argv);
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file(builder,"BSmap2.glade",NULL);
+	window = GTK_WIDGET(gtk_builder_get_object(builder,"window_main"));
+	gtk_builder_connect_signals(builder,NULL);
+	
+	// connection des widgets, on peut en faire une structure pour mettre tout ça dans le .h
+	pComboCal = GTK_WIDGET(gtk_builder_get_object(builder,"pComboCal"));
+	pEntryCal = GTK_WIDGET(gtk_builder_get_object(builder,"pEntryCal"));
+	
+	g_object_unref(builder); 
+	gtk_widget_show(window);
+	gtk_main(); 
+	 
+	return 0;
 }
+
 int sendReceive(char *commande)
 {
 	char input;
@@ -92,15 +73,7 @@ int sendReceive(char *commande)
 	return 0;
 }
 
-// types de commande, pour l'instant
-// p ou P : demande de trame position
-// r ou R : demande de trame robot
-// v ou V : demande de trame asservissement
-// a ou A : demande de trame actionneurs
-// s ou s : demande de trame sensors
-// t ou T : consigne de cible (target), TP pour polaire, TR pour rotation
-// d ou D : consigne d'actionneur
-// x ou X : test, on attend un ack simple
+
 int encode(char *request)
 {
 	char tmpchar[3];
@@ -283,6 +256,7 @@ int encode(char *request)
 	}
 	return 0;
 }
+
 int decode(char *trame,int t)
 {
 	char calstr[20];
@@ -356,7 +330,7 @@ int decode(char *trame,int t)
 			curRob.color = trame[4];
 			curRob.count = (int)(trame[5])+((int)(trame[6]) << 8); // en top 20ms
 			curRob.type = trame[7];
-			curRob.score = (uint8_t) (trame[8]);
+			curRob.score = (char) (trame[8]);
 #ifdef GTK_USE
 //			sprintf(text,"score %d",curRob.score);
 //			gtk_label_set_text(GTK_LABEL((GtkWidget *)pLabel[HMI_LABEL_SCORE]),(const gchar *)text);
@@ -418,10 +392,8 @@ int decode(char *trame,int t)
 			memset(calstr,0,20);
 			for (int i=0;i<(int)strlen(trame)-5;i++)
 				calstr[i]=trame[i+3];
-			printf("%s = %s\n",calname,calstr);
-#ifdef GTK_USE
-//			gtk_entry_set_text(GTK_ENTRY(pEntryCal),(const gchar *)calstr);
-#endif
+//			printf("%s = %s\n",calname,calstr);
+			gtk_entry_set_text(GTK_ENTRY(pEntryCal),(const gchar *)calstr);
 			break;
 		case ID_ACK:
 			printf("Ack reçu\n");
@@ -436,20 +408,45 @@ int decode(char *trame,int t)
 	return 0;
 }
 
-// calcule le checksum, à savoir la somme de tous les octets, casté en char
-char checkSum()
+// Thread qui va gérer l'envoi de messages en fonction de l'état de pollTrame
+
+void *manageMessages( void *d)
 {
-	int cs=0;
-	for (int i=0;i<strWrite[1]+3;i++)
-		cs+=(int)strWrite[i];
-	return (char)cs;
+#ifndef GTK_USE
+//	pollTrame[HMI_BUTTON_POSITION]=1;
+//	pollTrame[HMI_BUTTON_MOTORS]=1;
+//	pollTrame[HMI_BUTTON_ROBOT]=1;
+	//pollTrame[HMI_BUTTON_ASSERV]=1;
+	//pollTrame[HMI_BUTTON_POSITION]=1;
+#endif
+	while(1)
+	{
+		if (pollTrame[0])
+			sendReceive((char *)"P");//position
+		if (pollTrame[1])
+			sendReceive((char *)"M");//motors
+		if (pollTrame[2])
+			sendReceive((char *)"R");//robot
+		if (pollTrame[3])
+			sendReceive((char *)"V");//asserv
+			//sendReceive((char *)"S");//sensors
+			sleeps(0.5);//on patiente
+		//printf("inputready = %d\n",inputReady);
+		if (inputReady == 0) // on a écrit un truc
+		{
+			sendReceive(strInput);
+			inputReady = 1; // on autorise à nouveau l'input
+		}
+	}
+	return NULL;
 }
 
-/***** threads  ******/
-void *inputCommand( void*)
+// Thread qui va gérer l'entrée stdio pour envoyer des trames hors IHM
+
+void *inputCommand( void*g)
 {
 	//string s1;
-	char *s1;
+	//char *s1;
 	// thread qui va lire l'entrée standard 
 	while(1)
 	{
@@ -458,10 +455,10 @@ void *inputCommand( void*)
 		//	getline(std::cin, s1); // pour récupérer même les espaces
 		//	memcpy(strInput,s1.c_str(),s1.size());
 		//	strInput[s1.size()] = '\0';
-			if (strInput[0]=='l' || strInput[0] == 'L') // demande de log on/off
-				log = !log;
-			else
-				inputReady = false; // on va attendre que la commande soit envoyée
+		//	if (strInput[0]=='l' || strInput[0] == 'L') // demande de log on/off
+		//		logg = !logg;
+		//	else
+		//		inputReady = 0; // on va attendre que la commande soit envoyée
 				
 		}
 		sleeps(0.5);
@@ -470,239 +467,68 @@ void *inputCommand( void*)
     return NULL;
 }
 
-void *manageMessages( void*)
+char checkSum()
 {
-#ifndef GTK_USE
-//	pollTrame[HMI_BUTTON_POSITION]=true;
-//	pollTrame[HMI_BUTTON_MOTORS]=true;
-//	pollTrame[HMI_BUTTON_ROBOT]=true;
-	//pollTrame[HMI_BUTTON_ASSERV]=true;
-	//pollTrame[HMI_BUTTON_POSITION]=true;
-#endif
-	while(1)
-	{
-		if (pollTrame[HMI_BUTTON_POSITION])
-			sendReceive((char *)"P");//position
-		if (pollTrame[HMI_BUTTON_MOTORS])
-			sendReceive((char *)"M");//motors
-		if (pollTrame[HMI_BUTTON_ROBOT])
-			sendReceive((char *)"R");//robot
-		if (pollTrame[HMI_BUTTON_ASSERV])
-			sendReceive((char *)"V");//asserv
-			//sendReceive((char *)"S");//sensors
-			sleeps(0.5);//on patiente
-		
-		if (!inputReady) // on a écrit un truc
-		{
-			sendReceive(strInput);
-			inputReady = true; // on autorise à nouveau l'input
-		}
-	}
-	return NULL;
+	int cs=0;
+	for (int i=0;i<strWrite[1]+3;i++)
+		cs+=(int)strWrite[i];
+	return (char)cs;
 }
 
-// interface graphique
-int createHMI(void)
-{
-#ifdef GTK_USE
-
-//	c_red.alpha =1;c_red.red = 1;c_red.green = 0;c_red.blue=0;
-//	c_blue.alpha =1;c_blue.red = 0;c_blue.green = 0;c_blue.blue=1;
-//	c_green.alpha =1;c_green.red = 0;c_green.green = 1;c_green.blue=0;
-
-	builder = gtk_builder_new();
-	gtk_builder_add_from_file(builder,"window_main.glade",NULL);
-	window = GTK_WIDGET(gtk_builder_get_object(builder,"window_main"));
-	gtk_builder_connect_signals(builder,NULL);
-	
-	g_object_unref(builder);
-
-	// la fenêtre principale
-	//GtkWidget *pQuitBtn;
-	//gchar *TexteConverti;
-	
-	/*
-	// paramètres de la fenêtre
-	pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(pWindow),"BSmap by Brickstory");
-	gtk_window_set_default_size(GTK_WINDOW(pWindow),700,700);
-	g_signal_connect(G_OBJECT(pWindow),"delete-event",G_CALLBACK(gtk_main_quit),NULL);
-	// table
-	pTable=gtk_table_new(18,10,FALSE); //homogène
-	//gtk_widget_
-	gtk_container_add(GTK_CONTAINER(pWindow),GTK_WIDGET(pTable));
-	
-	pButton[HMI_BUTTON_POSITION]=gtk_button_new_with_label("Pos");
-	pButton[HMI_BUTTON_ASSERV]=gtk_button_new_with_label("Ass"); 
-	pButton[HMI_BUTTON_ROBOT]=gtk_button_new_with_label("Rob");
-	pButton[HMI_BUTTON_MOTORS]=gtk_button_new_with_label("Mot");
-	for (int j=0;j<4;j++)
-		gtk_widget_override_color(GTK_WIDGET(pButton[j]),GTK_STATE_FLAG_NORMAL, &c_blue);
-	// label
-	pLabel[HMI_LABEL_POS_POS]=gtk_label_new("POSITION\nx = \ny = \na = ");
-	//pLabel[HMI_LABEL_POS_Y]=gtk_label_new("y= ");
-	//pLabel[HMI_LABEL_POS_ALPHA]=gtk_label_new("a= ");
-	pLabel[HMI_LABEL_ASS_TAR]=gtk_label_new(" TARGET  \nx = \ny = \na = \n");
-	pLabel[HMI_LABEL_MOT_MOT]=gtk_label_new(" MOTORS  \nLeft = \nRight = \n");
-	//pLabel[HMI_LABEL_TAR_ALPHA]=gtk_label_new("tar a= ");
-	pLabel[HMI_LABEL_SCORE]=gtk_label_new("Score ");
-	pLabel[HMI_LABEL_STATE]=gtk_label_new("State ");
-	pLabel[HMI_LABEL_COLOR]=gtk_label_new("Color");
-	pLabel[HMI_LABEL_TIMER]=gtk_label_new("t = ");
-	//pLabel[HMI_LABEL_TAR_TYPE]=gtk_label_new("type =");
-	
-	pEntry = gtk_entry_new(); // pour rentrer une commande directe
-	
-	pComboCal = gtk_combo_box_text_new();
-	for (int i=0;i<NB_CAL;i++)
-		gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(pComboCal),NULL,listCal[i]);
-	pEntryCal = gtk_entry_new(); // l'input de la valeur de calib
-	
-	pDraw = gtk_drawing_area_new();
-	gtk_widget_set_size_request(pDraw,300,210);
-	// attachement des widgets dans la table
-	gtk_table_attach(GTK_TABLE(pTable),pButton[HMI_BUTTON_POSITION],13,14,1,2,GTK_EXPAND,GTK_EXPAND,0,0);
-	gtk_table_attach(GTK_TABLE(pTable),pButton[HMI_BUTTON_ASSERV],14,15,1,2,GTK_EXPAND,GTK_EXPAND,0,0);
-	gtk_table_attach(GTK_TABLE(pTable),pButton[HMI_BUTTON_ROBOT],15,16,1,2,GTK_EXPAND,GTK_EXPAND,0,0);
-	gtk_table_attach(GTK_TABLE(pTable),pButton[HMI_BUTTON_MOTORS],16,17,1,2,GTK_EXPAND,GTK_EXPAND,0,0);
-
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_SCORE],0,2,1,2);
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_STATE],3,5,1,2);
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_COLOR],6,8,1,2);
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_TIMER],9,11,1,2);
-
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_POS_POS],13,15,2,3);
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_MOT_MOT],13,15,3,4);
-
-	gtk_table_attach(GTK_TABLE(pTable),pComboCal,13,15,4,5,GTK_EXPAND,GTK_EXPAND,0,0);
-	gtk_table_attach(GTK_TABLE(pTable),pEntryCal,15,16,4,5,GTK_EXPAND,GTK_EXPAND,0,0);
-
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_ASS_TAR],16,18,2,3);
-	//gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_TAR_Y],16,18,3,4);
-	//gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_TAR_ALPHA],16,18,4,5);
-	//gtk_table_attach_defaults(GTK_TABLE(pTable),pLabel[HMI_LABEL_TAR_TYPE],16,18,5,6);
-
-	gtk_table_attach(GTK_TABLE(pTable),pEntry,0,3,10,11,GTK_EXPAND,GTK_EXPAND,0,0);
-	gtk_table_attach_defaults(GTK_TABLE(pTable),pDraw,0,10,5,10);
-//	gtk_widget_override_background_color(GTK_WIDGET(pLabel),GTK_STATE_FLAG_NORMAL, &c_blue);
-//	gtk_widget_override_color(GTK_WIDGET(pLabel),GTK_STATE_FLAG_NORMAL, &c_red);
-	for (int i=0;i<4;i++){
-		g_signal_connect(G_OBJECT(pButton[i]),"clicked",G_CALLBACK(change),NULL);}
-	g_signal_connect(G_OBJECT(pEntry),"activate",G_CALLBACK(getEntry),NULL);
-	g_signal_connect(G_OBJECT(pComboCal),"changed",G_CALLBACK(getCalValue),NULL);
-	g_signal_connect(G_OBJECT(pEntryCal),"activate",G_CALLBACK(setCalValue),NULL);
-//	g_signal_connect(G_OBJECT(pDraw),"expose_event",G_CALLBACK(redraw),NULL);
-	g_signal_connect(G_OBJECT(pDraw),"realize",G_CALLBACK(redraw2),NULL);
-	//redraw2(pDraw,NULL);*/
-/*	//gtk_container_add(GTK_CONTAINER(pWindow),pLabel);
-
-	pQuitBtn = gtk_button_new_with_label("Quitter");
-	g_signal_connect(G_OBJECT(pQuitBtn),"clicked",G_CALLBACK(gtk_main_quit),NULL);
-	gtk_container_add(GTK_CONTAINER(pWindow),pQuitBtn);
-*/
-	//gtk_widget_show_all(pWindow);
-	gtk_widget_show(window);
-#endif
-	return 0;
-}
-
-#ifdef GTK_USE
-/*void change(GtkWidget *pb,gpointer data)
-{
-	for (int i=0;i<NB_TRAMES;i++)
-	{
-		if (pb == pButton[i])
-		{
-			//printf("chg %d\n",i);
-			pollTrame[i] = ! pollTrame[i];
-		}
-	}// on découpe en deux sinon ça marche pas... je sais pas pourquoi
-	for (int i=0;i<NB_TRAMES;i++)
-	{
-		if (pollTrame[i])
-			gtk_widget_override_color(GTK_WIDGET(pButton[i]),GTK_STATE_FLAG_NORMAL, &c_red);
-		else
-			gtk_widget_override_color(GTK_WIDGET(pButton[i]),GTK_STATE_FLAG_NORMAL, &c_blue);
-			// rajouter le changement de couleur du bouton
-	}
-//		gtk_label_set_text(GTK_LABEL((GtkWidget *)data),(const gchar *)("salut"));
-//	if (pb == pButton[1])
-//		gtk_label_set_text(GTK_LABEL((GtkWidget *)data),(const gchar *)("coucou"));
-
-}
 void getEntry(GtkEntry *entry, gpointer data)
 {
-	char *txt;//[50];// = NULL;//(char *)malloc(50);
-	//memset(txt,0,50);
+	char *txt;
 	memset(strInput,0,50);
 	txt = (char *)gtk_entry_get_text(GTK_ENTRY(entry));
-	//memcpy(strInput,(char *)gtk_entry_get_text(GTK_ENTRY(entry)),strlen((char *)gtk_entry_get_text(GTK_ENTRY(entry))));
 	memcpy(strInput,txt,strlen(txt));
 	printf("string envoyée %s\n",txt);
-	inputReady = false;
-//	free(txt);
+	inputReady = 0;
 }
-void setCalValue(GtkEntry *entry, gpointer data)
+
+void setCalValue(GtkEntry *entry)
 {
-	//char *txt;//[50];// = NULL;//(char *)malloc(50);
-	//memset(txt,0,50);
 	sprintf(strInput,"CS %s %s",
 		gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(pComboCal)),
 		gtk_entry_get_text(GTK_ENTRY(entry)));
-//	memset(strInput,0,50);
-	//txt = (char *)gtk_entry_get_text(GTK_ENTRY(entry));
-	//memcpy(strInput,(char *)gtk_entry_get_text(GTK_ENTRY(entry)),strlen((char *)gtk_entry_get_text(GTK_ENTRY(entry))));
-	//memcpy(strInput,txt,strlen(txt));
-	//printf("string envoyée %s\n",txt);
-	inputReady = false;
-//	free(txt);
+	printf("cal\n");
+	inputReady = 0;
 }
 
-
-void getCalValue(GtkEntry *comb, gpointer data)
-{
-	//printf("%s\n",gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(comb)));
-	sprintf(strInput,"CG %s",gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(comb)));
-	inputReady = false; // pour lancer l'envoi
-}
-
-gboolean redraw2(GtkWidget *widget,gpointer userdata)
-{
-	cairo_t *cr=NULL;
-	cr=gdk_cairo_create(gtk_widget_get_window(widget));
-	cairo_set_source_rgb(cr,.1,.0,.0);
-	cairo_set_line_width(cr,2.0);
-	cairo_move_to(cr,10.,10.);
-	cairo_line_to(cr,100.,100.);
-	cairo_stroke(cr);
-	cairo_destroy(cr);
-	return false;
-}
-
-const gchar *getTextAssTar()
-{
-	//char txt[50];
-	sprintf(text," TARGET  \nx = %4d\ny = %4d\na = %4d\n",curAss.tarX,curAss.tarY, curAss.tarAlpha);
-	switch(curAss.type)
-	{
-		case ASS_NUL :
-		default :
-			strcat(text,"   NUL");break;
-		case ASS_POLAR :
-			strcat(text,"  POL AV");break;
-		case ASS_POLARREV :
-			strcat(text,"  POL_AR");break;
-		case ASS_ROTATION :
-			strcat(text," ROTATION");break;
-		case ASS_MANUAL :
-			strcat(text,"ASS_MANUAL");break;
-	}
-	return (const gchar *)text;
-}
-*/
+// callback de destruction de la fenêtre
 void on_window_main_destroy()
 {
+	
 	gtk_main_quit();
 }
 
-#endif
+// callback qui gère les boutons de demande de poll des trames
+void on_Button_toggled(GtkToggleButton *togg)
+{
+	gboolean state= gtk_toggle_button_get_active(togg);
+	char label[3];
+	int index;
+	strncpy(label,gtk_button_get_label(GTK_BUTTON(togg)),3);
+	if (strncmp(label,"Pos",3)==0)
+		index = HMI_BUTTON_POSITION;
+	else if (strncmp(label,"Mot",3)==0)
+		index = HMI_BUTTON_MOTORS;
+	else if (strncmp(label,"Ass",3)==0)
+		index = HMI_BUTTON_ASSERV;
+	else if (strncmp(label,"Sen",3)==0)
+		index = HMI_BUTTON_SENSOR;
+	else if (strncmp(label,"Rob",3)==0)
+		index = HMI_BUTTON_ROBOT;
+	else if (strncmp(label,"Act",3)==0)
+		index = HMI_BUTTON_ACTION;
+	if (state)
+		pollTrame[index]=1;
+	else
+		pollTrame[index] = 0;
+}
+
+void on_pComboCal_changed(GtkWidget *combo)
+{
+	sprintf(strInput,"CG %s",gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo)));
+	inputReady = 0; // pour lancer l'envoi
+//	printf("cahnge %s\n",gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo)));
+}
--- BSmap2.h
+++ BSmap2.h
@@ -1,8 +1,4 @@
-#ifndef __BSmap_h__
-#define __BSmap_h__
-
-
-//#include <iostream>
+//#include <iostream> // avec g++
 //using namespace std;
 #include <pthread.h>
 #include <unistd.h>
@@ -13,6 +9,13 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <wiringPi.h>
+//#define uint8_t char
+//#define bool char
+//#define true 1
+//#define false 0
+
+#define max(a,b) (a>b?a:b)
+#define toupper(g) g
 #define sleeps(t) usleep(t*1000000)
 
 #define STREMOTE_WAITINI 1
@@ -61,6 +64,13 @@
 #define ASS_CIRCLE 7
 #define ASS_BLOCK 10
 
+#define HMI_BUTTON_POSITION 0
+#define HMI_BUTTON_ASSERV 1
+#define HMI_BUTTON_MOTORS 2
+#define HMI_BUTTON_ROBOT 3
+#define HMI_BUTTON_ACTION 4
+#define HMI_BUTTON_SENSOR 5
+
 struct s_Position{
 	short posX;
 	short posY;
@@ -83,7 +93,7 @@ struct s_Robot{
 	char color;
 	int count;
 	char type;
-	uint8_t score;
+	char score;
 };
 struct s_Action{
 	char state1;
@@ -92,8 +102,8 @@ struct s_Action{
 struct s_Motors{
 	short spdRight;
 	short spdLeft;
-	uint8_t powerRight;
-	uint8_t powerLeft;
+	char powerRight;
+	char powerLeft;
 };
 
 struct s_Sensors{
@@ -115,7 +125,7 @@ struct s_Sensors curSen;
 int encode(char *request);
 int decode(char *trame,int t);
 void *inputCommand( void*);
-void *manageMessages( void*);
+void *manageMessages( void* d);
 
 int sendReceive(char *commande);
 char checkSum();
@@ -123,76 +133,32 @@ char strWrite[100];
 char strRead[50];
 int sizeWrite;
 int sizeRead;
-bool log = false;
+char logg = 0;
 char VersionRobot=17; // 0x01 = 2021 + 0x10=  principal
 char calname[20];
 char strInput[50];
-bool inputReady = true;
+int inputReady = 1;
 
 pthread_t inp;
 pthread_t mess;
 
-// interface graphique
-#define NB_TRAMES 4
-#define NB_LABELS 12
-#define NB_CAL 32
-
-#define HMI_BUTTON_POSITION 0
-#define HMI_BUTTON_ASSERV 1
-#define HMI_BUTTON_MOTORS 2
-#define HMI_BUTTON_ROBOT 3
-#define HMI_BUTTON_ACTION 4
-
-#define HMI_LABEL_POS_POS 0
-#define HMI_LABEL_POS_VIT 1
-#define HMI_LABEL_ASS_TAR 2
-#define HMI_LABEL_ASS_SPD 7
-#define HMI_LABEL_MOT_MOT 8
-
-
-#define HMI_LABEL_SCORE 3
-#define HMI_LABEL_STATE 4
-#define HMI_LABEL_COLOR 5
-#define HMI_LABEL_TIMER 6
-//#define HMI_LABEL_TAR_X 7
-//#define HMI_LABEL_TAR_Y 8
-//#define HMI_LABEL_TAR_ALPHA 9
-//#define HMI_LABEL_TAR_TYPE 10
-
 char text[50];
 
 int createHMI(void);
-bool pollTrame[NB_TRAMES];
-#ifdef GTK_USE
+char pollTrame[5]={0,0,0,0,0};
 void change(GtkWidget *pb,gpointer data);
 const gchar *getTextAssTar();
 void getEntry(GtkEntry *entry, gpointer data);
 void getCalValue(GtkEntry *comb, gpointer data);
-void setCalValue(GtkEntry *entry, gpointer data);
-gboolean redraw2(GtkWidget *widget,gpointer userdata);
-void on_window_main_destroy();
-
-GtkWidget *pWindow;
-GtkBuilder *builder;
-GtkWidget *window;
-GtkWidget *pLabel[20];
-GtkWidget *pTable;
-GtkWidget *pButton[NB_TRAMES];
-GtkWidget *pEntry;
-GtkWidget *pEntryCal;
+//void setCalValue(GtkEntry *entry, gpointer data);
+//gboolean redraw2(GtkWidget *widget,gpointer userdata);
+//void on_window_main_destroy();
+GtkWidget *PosButton;
 GtkWidget *pComboCal;
-GtkWidget *pDraw;
-GdkRGBA c_red,c_blue,c_green;
-const char *listCal[NB_CAL] = {"COEFF_SPD_FOR","COEFF_SPD_ROT","COEFF_ANG",
-	"COEFF_MM","DIST_CONVERGE","ANGLE_CONVERGE","MMSROB_TO_DPSWHL",
-	"DPSROB_TO_DPSWHL","KP_FOR","KP_ROT","VERSION","WHEEL_RIGHT",
-	"WHEEL_LEFT","SCORE_INIT","ENCODER_LEFT","ENCODER_RIGHT",
-	"EXTERNAL_ENCODER","NUMBER_FRONT_SONAR","NUMBER_REAR_SONAR",
-	"SONAR_FRONT_LEFT","SONAR_FRONT_RIGHT","SONAR_REAR_LEFT",
-	"SONAR_REAR_RIGHT","TIRETTE_HW","TIRETTE_PORT,""TIRETTE_EV3",
-	"BAU_HW","BAU_PORT","BAU_EV3","COLOR_HW","COLOR_PORT","COLOR_EV3"};
+GtkWidget *pEntryCal;
 
-//cairo_t *cr;
-#endif
+const char * fiforead = "/tmp/RobBSmap"; // du robot vers l'outil
+const char * fifowrite = "/tmp/BSmapRob"; // de l'outil vers le robot
+int fd;
 
-#endif
+char blabla = 1;
